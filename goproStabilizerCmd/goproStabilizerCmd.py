3016d5abf6cd5b1ea209429946edcc132fc99e60
huckvrni <nhajeho1@gmail.com>
2019-07-04 16:08:11 +0300

    First commit

---
+++ goproStabilizerCmd/goproStabilizerCmd.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+'''
+goproStabilizerCmd.goproStabilizerCmd -- shortdesc
+
+goproStabilizerCmd.goproStabilizerCmd is a description
+
+It defines classes_and_methods
+
+@author:     yuval herman
+
+@copyright:  2019 yuval herman. All rights reserved.
+
+@license:    license
+
+@deffield    updated: Updated
+'''
+
+import sys,subprocess
+from pathlib import Path
+from gpmfPy.gpmfPy import gpmfStream
+from pprint import pprint
+
+if not len(sys.argv) > 1:
+    print("you have to at leat input one video.\nUsage: goproStabilize input.mp4")
+
+if not Path(sys.argv[1]).is_file():
+    print("File not found or it does not exist")
+
+# subprocess.run(["ffmpeg", "-y", "-i", sys.argv[1], "-codec", "copy", "-map", "0:3", "-f", "rawvideo", "out.bin"])
+
+with open("out.bin", "rb") as f:
+    hexData = f.read()
+
+gpmf = gpmfStream(hexData)
+with open('output.txt', 'wt') as out:
+    pprint(gpmf.getGpmfAt(0), stream=out)
---
+++ gpmfPy/gpmfPy.py
@@ -0,0 +1,125 @@
+'''
+Created on 3 Jul 2019
+
+@author: herman
+'''
+from enum import Enum
+from _ast import If
+
+class GPMF_TYPE(Enum):
+	GPMF_TYPE_STRING_ASCII = 'c' #single byte 'c' style character string
+	GPMF_TYPE_SIGNED_BYTE = 'b'#single byte signed number
+	GPMF_TYPE_UNSIGNED_BYTE = 'B' #single byte unsigned number
+	GPMF_TYPE_SIGNED_SHORT = 's'#16-bit integer
+	GPMF_TYPE_UNSIGNED_SHORT = 'S'#16-bit integer
+	GPMF_TYPE_FLOAT = 'f' #32-bit single precision float (IEEE 754)
+	GPMF_TYPE_FOURCC = 'F' #32-bit four character tag 
+	GPMF_TYPE_SIGNED_LONG = 'l'#32-bit integer
+	GPMF_TYPE_UNSIGNED_LONG = 'L' #32-bit integer
+	GPMF_TYPE_Q15_16_FIXED_POINT = 'q' # Q number Q15.16 - 16-bit signed integer (A) with 16-bit fixed point (B) for A.B value (range -32768.0 to 32767.99998). 
+	GPMF_TYPE_Q31_32_FIXED_POINT = 'Q' # Q number Q31.32 - 32-bit signed integer (A) with 32-bit fixed point (B) for A.B value. 
+	GPMF_TYPE_SIGNED_64BIT_INT = 'j' #64 bit signed long
+	GPMF_TYPE_UNSIGNED_64BIT_INT = 'J' #64 bit unsigned long	
+	GPMF_TYPE_DOUBLE = 'd' #64 bit double precision float (IEEE 754)
+	GPMF_TYPE_UTC_DATE_TIME = 'U' #128-bit ASCII Date + UTC Time format yymmddhhmmss.sss - 16 bytes ASCII (years 20xx covered)
+	GPMF_TYPE_GUID = 'G' #128-bit ID (like UUID)
+
+	GPMF_TYPE_COMPLEX = '?' #for sample with complex data structures, base size in bytes.  Data is either opaque, or the stream has a TYPE structure field for the sample.
+
+	GPMF_TYPE_NEST = 0 # used to nest more GPMF formatted metadata 
+
+class gpmfStream(object):
+	'''
+	classdocs
+	'''
+	bytesArray = []
+
+	def __init__(self, bytesArray):
+		'''
+		Constructor
+		'''
+		self.bytesArray = bytesArray
+	
+	def getGpmfAt(self, i, end=0):
+		if i+8 > len(self.bytesArray):
+			raise IndexError("gpmf index out of range")
+		if end != 0:
+			gpmfKLV = []
+			while i < end:
+				key = str(self.bytesArray[i:i+4]) #chr(self.bytesArray[i])+chr(self.bytesArray[i+1])+chr(self.bytesArray[i+2])+chr(self.bytesArray[i+3])
+				i+=4
+				ltype = self.bytesArray[i]
+				lsize = self.bytesArray[i+1]
+				lrepeat = int(hex(self.bytesArray[i+2])[2:] + hex(self.bytesArray[i+3])[2:], 16)
+				padds = 0
+				i+=4
+				if ltype == GPMF_TYPE.GPMF_TYPE_NEST.value:
+					end=lsize*lrepeat+padds
+					gpmfKLV = [key, chr(ltype), lsize*lrepeat+padds, self.getGpmfAt(i, end)]
+					break
+				if self.bytesArray[i+(lsize*lrepeat)] == 0: #chr(ltype) == GPMF_TYPE.GPMF_TYPE_STRING_ASCII.value and lsize == 1:
+					padds = 2
+				gpmfKLV.append([key, chr(ltype), lsize*lrepeat+padds, self.bytesArray[i:i+(lsize*lrepeat+padds)]])
+				i+=lsize*lrepeat+padds
+			return(gpmfKLV)
+		
+		key = str(self.bytesArray[i:i+4]) #chr(self.bytesArray[i])+chr(self.bytesArray[i+1])+chr(self.bytesArray[i+2])+chr(self.bytesArray[i+3])
+		i+=4
+		ltype = self.bytesArray[i]
+		lsize = self.bytesArray[i+1]
+		lrepeat = int(hex(self.bytesArray[i+2])[2:] + hex(self.bytesArray[i+3])[2:], 16)
+		padds = 0
+		i+=4
+		
+		if ltype == GPMF_TYPE.GPMF_TYPE_NEST.value:
+			end=lsize*lrepeat+padds
+			gpmfKLV = [key, chr(ltype), lsize*lrepeat+padds, self.getGpmfAt(i, end)]
+		else:
+			gpmfKLV = [key, chr(ltype), lsize*lrepeat+padds, self.bytesArray[i:i+(lsize*lrepeat+padds)]]
+		if end == 0:
+			return(gpmfKLV)
+		else:
+			i += lsize*lrepeat+padds
+		return(gpmfKLV)
+	
+	def gpmfToList(self):
+		i=0
+		gpmfList = []
+		while True:
+			if i+8 > len(self.bytesArray):
+				break
+			key = chr(self.bytesArray[i])+chr(self.bytesArray[i+1])+chr(self.bytesArray[i+2])+chr(self.bytesArray[i+3])
+			i+=4
+			ltype = self.bytesArray[i]
+			lsize = self.bytesArray[i+1]
+			lrepeat = int(hex(self.bytesArray[i+2])[2:] + hex(self.bytesArray[i+3])[2:], 16)
+			padds = 0
+			i+=4
+			if ltype == GPMF_TYPE.GPMF_TYPE_NEST.value:
+				oldKey = key
+				oldLen = lsize*lrepeat+padds
+				nestList = []
+				while True:
+					if i+8 > len(self.bytesArray):
+						break
+					key = chr(self.bytesArray[i])+chr(self.bytesArray[i+1])+chr(self.bytesArray[i+2])+chr(self.bytesArray[i+3])
+					i+=4
+					ltype = self.bytesArray[i]
+					lsize = self.bytesArray[i+1]
+					lrepeat = int(hex(self.bytesArray[i+2])[2:] + hex(self.bytesArray[i+3])[2:], 16)
+					i+=4
+					nestList.append([key, lsize*lrepeat+padds, self.bytesArray[i:i+(lsize*lrepeat+padds)]])
+					i += lsize*lrepeat+padds
+				gpmfList.append([oldKey, oldLen, nestList])
+			else:
+				gpmfList.append([key, lsize*lrepeat+padds, self.bytesArray[i:i+(lsize*lrepeat+padds)]])
+				i += lsize*lrepeat+padds
+			
+# 			print(key)
+# 			print(chr(ltype)== GPMF_TYPE.GPMF_TYPE_NEST)
+# 			print(lsize)
+# 			print(lrepeat)
+# 			print("-------------")
+# 		print(len(gpmfList))
+		for i in gpmfList:
+			print(i)
