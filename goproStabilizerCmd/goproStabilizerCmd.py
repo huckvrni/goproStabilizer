a07a103a3183f4541fb3d2726997068b6c09f4f4
huckvrni <nhajeho1@gmail.com>
2019-07-07 16:01:24 +0300

    commit

--- goproStabilizerCmd/goproStabilizerCmd.py
+++ goproStabilizerCmd/goproStabilizerCmd.py
@@ -28,6 +28,32 @@ def twos_complement(hexstr,bits):
         value -= 1 << bits
     return value
 
+def split(a, n):
+    if type(n) is int:
+        k, m = divmod(len(a), n)
+        return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
+    elif type(n) is float:
+        ret = []
+        temp = []
+        for i, item in enumerate(a):
+            if len(temp) == 0 and len(a)-i < int(n):
+                temp = a[i:]
+                ret.append(temp)
+                return(ret)
+            if len(temp) < int(n):
+                temp.append(item)
+            else:
+                ret.append(temp)
+                temp = []
+        return(ret)
+    else:
+        raise ValueError("expected int or float")
+
+def printGpmfToFile():
+    with open('output.txt', 'wt') as out:
+        gpmfList = gpmf.getGpmfList()
+    pprint(gpmfList, stream=out)
+
 if not len(sys.argv) > 1:
     print("you have to at leat input one video.\nUsage: goproStabilize input.mp4")
 
@@ -36,15 +62,24 @@ if not Path(sys.argv[1]).is_file():
 
 
 # subprocess.run(["ffmpeg", "-y", "-i", sys.argv[1], "-codec", "copy", "-map", "0:3", "-f", "rawvideo", "out.bin"])
+process = subprocess.Popen(["ffprobe", "-v", "0", "-of", "csv=p=0", "-select_streams", "v:0", "-show_entries", "stream=r_frame_rate", sys.argv[1]], stdout=subprocess.PIPE)
+out, err = process.communicate()
+rate = out[:-1].decode("utf-8").split('/')
+if len(rate)==1:
+    rate = float(rate[0])
+elif len(rate)==2:
+    rate = float(rate[0])/float(rate[1])
+else:
+    raise ValueError("framerate couldn't be found.")
+
 # os.makedirs("/tmp/goprostabilizer", exist_ok=True)
 # subprocess.run(["ffmpeg", "-i", sys.argv[1], "/tmp/goprostabilizer/%d.tiff"])
 
-
 with open("out.bin", "rb") as f:
     hexData = f.read()
- 
+  
 gpmf = gpmfStream(hexData)
-
+ 
 gyroStreams = gpmf.getStream("GYRO")
 # print(gyroStreams)
 
@@ -53,6 +88,9 @@ for i, stream in enumerate(gyroStreams):
     x=0
     y=0
     z=0
+    samples = int(stream[0].hex(), 16) - int(gyroStreams[i-1][0].hex(), 16) if i > 0 else int(gyroStreams[0][0].hex(), 16)
+#     print(samples/rate)
+#     print([len(x) for x in split(stream[2], samples/rate)])
     for data in stream[2]:
         num+=1
         z+=twos_complement(data[0:1].hex(), 16) / twos_complement(stream[1].hex(), 16)
@@ -64,16 +102,8 @@ for i, stream in enumerate(gyroStreams):
     print("x:" + str(x) + "\n" + "y:" + str(y) + "\n" + "z:" + str(z) + "\n")
     print()
 
-# print(int(gpmf.getStream("GYRO")[0][0].hex() ,16))
-# for x in range(1, len(gpmf.getStream("GYRO"))-2):
-#     print(int(gpmf.getStream("GYRO")[x][0].hex() ,16) - int(gpmf.getStream("GYRO")[x-1][0].hex() ,16))
-
-
 #ffplay -i GOPR9173.mp4 -vf lenscorrection=k1=-0.5:k2=0.5
 
 # print(int(gpmf.getStream("GYRO")[0][-1][0].hex(), 16))
-# with open('output.txt', 'wt') as out:
-#     gpmfList = gpmf.getGpmfList()
-#     pprint(gpmfList, stream=out)
 
 print("done!")
